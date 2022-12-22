f890a7609fcb73c97f335b693053326cedf38bc0
Dalia-21 <dalia.sinclair@protonmail.com>
2022-12-22 17:56:01 +1100

    update lcd/keypad test to use new class

--- lcd_keypad_combo.py
+++ lcd_keypad_combo.py
@@ -1,41 +1,39 @@
-"""Basic program to test lcd screen and keypad working
-together."""
-
-import machine
+from machine import Pin
 import utime
-import gpio_lcd
+from gpio_lcd import GpioLcd
+from keypad import Keypad
+
+screen = GpioLcd(rs_pin=Pin(15),
+              enable_pin=Pin(14),
+              d4_pin=Pin(13),
+              d5_pin=Pin(12),
+              d6_pin=Pin(11),
+              d7_pin=Pin(10),
+              num_lines=2, num_columns=16)
+
+keypad_rows = [9, 8, 7, 6]
+keypad_cols = [3, 2, 1, 0]
+
+keypad = Keypad(keypad_rows, keypad_cols)
 
 
-# This code does not appear to currently include instructions
-# to transmit key input to lcd screen
-
-
-# two lists of gpio numbers
-def InitKeypad(rows, cols):
-    row_pins = [machine.Pin(pin_num, machine.Pin.OUT) for pin_num in rows]
-    col_pins = [machine.Pin(pin_num, machine.Pin.IN, pull=machine.Pin.PULL_DOWN) for pin_num in cols]
-    
-    for row in range(4):
-        row_pins[row].low()
-    
-# pass to timer init call        
-def PollKeypad(timer):
-    # definitions
-    KEY_UP = const(0)
-    KEY_DOWN = const(1)
-
-    keys = [['1', '2', '3', 'A'], ['4', '5', '6', 'B'],
-            ['7', '8', '9', 'C'], ['*', '0', '#', 'D']]
-    key = None
-
-    for row in range(4):
-        row_pins[row].high()
-        for col in range(4):
-            if col_pins[col].value() == KEY_DOWN:
-                key = KEY_DOWN
-            else:
-                key = KEY_UP
-            if key == KEY_DOWN:
-                lcd_print(keys[row][col])
-                
-
+while True:
+    screen.clear()
+    screen.move_to(0,0)
+    screen.putstr("Press a key: ")
+    screen.show_cursor()
+    utime.sleep(1)
+    key = keypad.get_keypress()
+    screen.clear()
+    screen.move_to(0,0)
+    screen.putstr("You pressed: ")
+    screen.putstr(key)
+    utime.sleep(1)
+    screen.clear()
+    screen.move_to(0,0)
+    screen.putstr("Press * ")
+    screen.move_to(0,1)
+    screen.putstr("to go again")
+    key = keypad.get_keypress()
+    while key != '*':
+        key = keypad.get_keypress()
