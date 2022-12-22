746dc6b99708a10a600e6911f50c8088b6840398
Dalia-21 <dalia.sinclair@protonmail.com>
2022-12-22 18:02:08 +1100

    move testing programs to tests directory

---
+++ tests/lcd_keypad_combo.py
@@ -0,0 +1,40 @@
+from machine import Pin
+import utime
+from gpio_lcd import GpioLcd
+from keypad import Keypad
+
+screen = GpioLcd(rs_pin=Pin(15),
+              enable_pin=Pin(14),
+              d4_pin=Pin(13),
+              d5_pin=Pin(12),
+              d6_pin=Pin(11),
+              d7_pin=Pin(10),
+              num_lines=2, num_columns=16)
+
+keypad_rows = [9, 8, 7, 6]
+keypad_cols = [3, 2, 1, 0]
+
+keypad = Keypad(keypad_rows, keypad_cols)
+
+
+while True:
+    screen.clear()
+    screen.move_to(0,0)
+    screen.putstr("Press a key: ")
+    screen.blink_cursor_on()
+    utime.sleep(1)
+    key = keypad.get_keypress()
+    screen.blink_cursor_off()
+    screen.clear()
+    screen.move_to(0,0)
+    screen.putstr("You pressed: ")
+    screen.putstr(key)
+    utime.sleep(1)
+    screen.clear()
+    screen.move_to(0,0)
+    screen.putstr("Press * ")
+    screen.move_to(0,1)
+    screen.putstr("to go again")
+    key = keypad.get_keypress()
+    while key != '*':
+        key = keypad.get_keypress()
