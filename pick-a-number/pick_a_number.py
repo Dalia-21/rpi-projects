fcf3bb58006bc9f8723bd845608da1b412953508
Dalia-21 <dalia.sinclair@protonmail.com>
2022-12-23 17:11:50 +1100

    initial commit

---
+++ pick-a-number/pick_a_number.py
@@ -0,0 +1,16 @@
+from machine import Pin
+from keypad import Keypad
+from gpio_lcd import GpioLcd
+import utime
+
+# initialise components
+keypad_rows = [9, 8, 7, 6]
+keypad_cols = [3, 2, 1, 0]
+
+keypad = Keypad(keypad_rows, keypad_cols)
+
+screen = GpioLcd(rs_pin=Pin(15), enable_pin=Pin(14),
+              d4_pin=Pin(13), d5_pin=Pin(12),
+              d6_pin=Pin(11), d7_pin=Pin(10))
+
+
