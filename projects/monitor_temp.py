10c037f2cafd34b7b647a104fc45e894b18bdfdb
Dalia-21 <dalia.sinclair@protonmail.com>
2022-12-23 20:27:45 +1100

    implement helper functions and main code loop

--- projects/monitor_temp.py
+++ projects/monitor_temp.py
@@ -1,4 +1,4 @@
-import machine
+from machine import Pin
 import utime
 import dht
 from keypad import Keypad
@@ -15,4 +15,45 @@ screen = GpioLcd(rs_pin=Pin(15), enable_pin=Pin(14),
               d4_pin=Pin(13), d5_pin=Pin(12),
               d6_pin=Pin(11), d7_pin=Pin(10))
 
-sensor = dht.DHT11(machine.Pin(4))
+sensor = dht.DHT11(Pin(4))
+
+
+# Define menu and sensor reading functions
+def display_readings(option):
+    sensor.measure()
+    if option == 'A':
+        screen.clear()
+        screen.putstr("Temp: {} Hum: {}".format(sensor.temperature(),
+                                                sensor.humidity()))
+    elif option == 'B':
+        screen.clear()
+        screen.putstr("Temp: {}".format(sensor.temperature()))
+    elif option == 'C':
+        screen.clear()
+        screen.putstr("Humidity: {}".format(sensor.humidity()))
+
+
+def menu(keypress):
+    menu_text = "Press A for temp and humidity, B for temp only, C for humidity only"
+    if keypress == '*':
+        screen.scroll_text(menu_text)
+    elif keypress in "ABC":
+        display_readings(keypress)
+    else:
+        screen.scroll_text("Unknown option selected. Press * to view menu options again.")
+
+
+# Display welcome text
+screen.clear()
+screen.scroll_text("Welcome to the Temperature Monitoring Program.")
+utime.sleep(1)
+screen.scroll_text("Press * to view menu options.")
+utime.sleep(1)
+
+
+# Enter main code loop
+while True:
+    screen.move_to(0,1)
+    screen.putstr("Press * for menu")
+    menu(keypad.get_keypress())
+    
